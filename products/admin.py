1d1c2cb028002292182a7c56baef1dc1ecc9b339
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2024-10-10 16:01:45 +0100

    feature: adding model to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('name',)
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('name',)
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'brand', 'category', 'is_active')
+    search_fields = ('name','brand__name', 'category__name')
+    list_filter = ('is_active', 'brand', 'category')
--- products/models.py
+++ products/models.py
@@ -1,3 +1,58 @@
+from venv import create
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+class Brand(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Marca')
+    description = models.TextField(null=True, blank=True, verbose_name='Descrição')
+    is_active = models.BooleanField(default=False, verbose_name='Ativo')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado em')
+
+    class Meta:
+        verbose_name = 'Marca'
+        verbose_name_plural = 'Marcas'
+        ordering = ["created_at", "name"]
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Categória')
+    description = models.TextField(null=True, blank=True, verbose_name='Descrição')
+    is_active = models.BooleanField(default=False, verbose_name='Ativo')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado em')
+
+    class Meta:
+        verbose_name = 'Categória'
+        verbose_name_plural = 'Categórias'
+        ordering = ["created_at", "name"]
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Produto')
+    description = models.TextField(null=True, blank=True, verbose_name='Descrição')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Preço')
+
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, verbose_name='Marca')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Categoria')
+
+    is_active = models.BooleanField(default=False, verbose_name='Ativo')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado em')
+
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+        ordering = ["created_at", "name"]
+
+    def __str__(self):
+        return self.name
